1fd32a2aec86f71c76d1cfeb6abad8b774cb1fd5
superV18 <vgarg6444@gmail.com>
2023-01-04 10:54:10 +0530

    till 3rd jan 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,19 @@ export class AppComponent {
   title = 'LearningAngular';
   displayValue:string='';
   displayValue2:string='';
+  count=0;
+  name="vishal Garg";
+  disable=false;
+  show=true;
+  color='green';
+  users=['Vishal', 'Gaurav', 'Sourav', 'keshav', 'Harish']
+  usersDetails=[
+    {name:'Vishal',email: 'V@mail.com', phone: '0000000000', socialAccount:['Facebook','Instagram','Whatsapp']},
+    {name:'Gaurav',email: 'G@mail.com', phone: '0000000001', socialAccount:['Facebook','Instagram','Whatsapp']},
+    {name:'Sourav',email: 'S@mail.com', phone: '0000000002', socialAccount:['Facebook','Instagram','Whatsapp']},
+    {name:'Keshav',email: 'K@mail.com', phone: '0000000003', socialAccount:['Facebook','Instagram','Whatsapp']},
+    {name:'Harish',email: 'H@mail.com', phone: '0000000004', socialAccount:['Facebook','Instagram','Whatsapp']}
+  ]
   getName1() {
     alert('Function Called');
   }
@@ -32,5 +45,8 @@ export class AppComponent {
   getInputReal1(value: string) {
     this.displayValue2=value;
     console.log(value);
+  }
+  counter(type:string){
+    type==='add' ?this.count++:this.count--;
   }
 }
