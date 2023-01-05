ddb40f302de8a840a3bdf25cdd2bf8f958bb4a71
superV18 <vgarg6444@gmail.com>
2023-01-05 12:02:06 +0530

    till 4th jan 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,21 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LearningAngular';
-  displayValue:string='';
-  displayValue2:string='';
-  count=0;
-  name="vishal Garg";
-  disable=false;
-  show=true;
-  color='green';
-  users=['Vishal', 'Gaurav', 'Sourav', 'keshav', 'Harish']
-  usersDetails=[
-    {name:'Vishal',email: 'V@mail.com', phone: '0000000000', socialAccount:['Facebook','Instagram','Whatsapp']},
-    {name:'Gaurav',email: 'G@mail.com', phone: '0000000001', socialAccount:['Facebook','Instagram','Whatsapp']},
-    {name:'Sourav',email: 'S@mail.com', phone: '0000000002', socialAccount:['Facebook','Instagram','Whatsapp']},
-    {name:'Keshav',email: 'K@mail.com', phone: '0000000003', socialAccount:['Facebook','Instagram','Whatsapp']},
-    {name:'Harish',email: 'H@mail.com', phone: '0000000004', socialAccount:['Facebook','Instagram','Whatsapp']}
-  ]
+  display= true;
+  displayValue: string = '';
+  displayValue2: string = '';
+  count = 0;
+  userData:any={};
+  name = 'vishal Garg';
+  disable = false;
+  show = true;
+  bgColor = 'aqua';
+  color = 'green';
+  users = ['Vishal', 'Gaurav', 'Sourav', 'keshav', 'Harish'];
+  usersDetails = [
+    {
+      name: 'Vishal',
+      email: 'V@mail.com',
+      phone: '0000000000',
+      socialAccount: ['Facebook', 'Instagram', 'Whatsapp'],
+    },
+    {
+      name: 'Gaurav',
+      email: 'G@mail.com',
+      phone: '0000000001',
+      socialAccount: ['Facebook', 'Instagram', 'Whatsapp'],
+    },
+    {
+      name: 'Sourav',
+      email: 'S@mail.com',
+      phone: '0000000002',
+      socialAccount: ['Facebook', 'Instagram', 'Whatsapp'],
+    },
+    {
+      name: 'Keshav',
+      email: 'K@mail.com',
+      phone: '0000000003',
+      socialAccount: ['Facebook', 'Instagram', 'Whatsapp'],
+    },
+    {
+      name: 'Harish',
+      email: 'H@mail.com',
+      phone: '0000000004',
+      socialAccount: ['Facebook', 'Instagram', 'Whatsapp'],
+    },
+  ];
   getName1() {
     alert('Function Called');
   }
@@ -39,14 +67,26 @@ export class AppComponent {
     console.log(value);
   }
   getInputReal(value: string) {
-    this.displayValue=value;
+    this.displayValue = value;
     console.log(value);
   }
   getInputReal1(value: string) {
-    this.displayValue2=value;
+    this.displayValue2 = value;
     console.log(value);
   }
-  counter(type:string){
-    type==='add' ?this.count++:this.count--;
+  counter(type: string) {
+    type === 'add' ? this.count++ : this.count--;
+  }
+  updateColor() {
+    this.color = 'blue';
+    this.bgColor = 'red';
+  }
+  getFormdata(data:NgForm) {
+    console.log(data);
+    alert('Hurrah!!! Your Form is Submitted');
+    this.userData=data;
+  }
+  toggleElement(){
+    this.display=!this.display; //it is called negation, used to toggle the value from false to true, from true to false on click
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserAuthModule } from './user-auth/user-auth.module';
 import { VishalComponent } from './vishal/vishal.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
-  declarations: [AppComponent, VishalComponent],
-  imports: [BrowserModule, AppRoutingModule, UserAuthModule],
+  declarations: [AppComponent, VishalComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, UserAuthModule, FormsModule, BrowserAnimationsModule, MatSlideToggleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
